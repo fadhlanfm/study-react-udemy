bcc4566fa087099b7b699d40ca5babc0ec745511
Fadhlan Fariz Makarim <fadhlanfariz95@gmail.com>
2021-11-30 11:46:46 +0700

    using stateful lists

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
-import ExpensesFilter from './components/Expenses/ExpensesFilter';
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
-  // return React.createElement(
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES); // return React.createElement(
   //   'div',
   //   {},
   //   React.createElement('h2', {}, 'Let's get started!'),
   //   React.createElement(Expenses, { items: expenses })
   // );
-  const addExpenseHandler = expense => {
-    console.log(expense)
-  }
+  const addExpenseHandler = (expense) => {
+    console.log(
+      "🚀 ~ file: App.js ~ line 38 ~ addExpenseHandler ~ expense",
+      expense
+    );
+    // setExpenses([expense, ...expenses]); // bad practice
+    setExpenses((prevExpenses) => {
+      console.log("prevExpenses", [expense, ...prevExpenses]);
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
@@ -43,6 +50,6 @@ const App = () => {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
